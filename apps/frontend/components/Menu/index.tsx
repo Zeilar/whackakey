5a771ed577f42c01bab2d665a5b0adc2508ee53b
Zeilar <philip_angelin@hotmail.com>
2022-08-05 22:25:15 +0200

    feat: a11y

--- apps/frontend/components/Menu/index.tsx
+++ apps/frontend/components/Menu/index.tsx
@@ -4,7 +4,7 @@ import { ArrowLeft } from "@styled-icons/evaicons-solid/ArrowLeft";
 import { ArrowRight } from "@styled-icons/evaicons-solid/ArrowRight";
 import useGameContext from "apps/frontend/hooks/useGameContext";
 import { DifficultyTiming } from "apps/frontend/types/game";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 interface DifficultyItemProps {
 	onChange(difficultyTiming: DifficultyTiming): void;
@@ -21,21 +21,50 @@ const difficultyColors = ["green.500", "yellow.500", "red.500"];
 
 function DifficultyItem({ onChange }: DifficultyItemProps) {
 	const [selected, setSelected] = useState(0);
+	const ref = useRef<HTMLDivElement>(null);
+
+	const previous = useCallback(() => {
+		setSelected(p => (selected - 1 in difficulties ? p - 1 : difficulties.length - 1));
+	}, [selected]);
+
+	const next = useCallback(() => {
+		setSelected(p => (selected + 1 in difficulties ? p + 1 : 0));
+	}, [selected]);
+
+	useEffect(() => {
+		function onKeyDown(e: KeyboardEvent) {
+			if (!ref.current?.contains(document.activeElement) || !["ArrowLeft", "ArrowRight"].includes(e.key)) {
+				return;
+			}
+			if (e.key === "ArrowLeft") {
+				previous();
+			}
+			if (e.key === "ArrowRight") {
+				next();
+			}
+		}
+		document.addEventListener("keydown", onKeyDown);
+		return () => {
+			document.removeEventListener("keydown", onKeyDown);
+		};
+	}, [next, previous]);
 
 	useEffect(() => {
 		onChange(difficultiesMap[difficulties[selected]]);
 	}, [selected, onChange]);
 
-	function previous() {
-		setSelected(p => (selected - 1 in difficulties ? p - 1 : difficulties.length - 1));
-	}
-
-	function next() {
-		setSelected(p => (selected + 1 in difficulties ? p + 1 : 0));
-	}
-
 	return (
-		<Button as="div" variant="key" size="xl" justifyContent="space-between" px={8} _hover={{}}>
+		<Button
+			// @ts-expect-error type bug
+			ref={ref}
+			as="div"
+			variant="key"
+			size="xl"
+			tabIndex={0}
+			justifyContent="space-between"
+			px={8}
+			_hover={{}}
+		>
 			<IconButton
 				variant="unstyled"
 				aria-label="previous difficulty"
@@ -44,7 +73,6 @@ function DifficultyItem({ onChange }: DifficultyItemProps) {
 				bgColor="blue.700"
 				color="gray.100"
 				onClick={previous}
-				_focus={{}}
 			/>
 			<Text color={difficultyColors[selected]} mx={10}>
 				{difficulties[selected]}
@@ -57,7 +85,6 @@ function DifficultyItem({ onChange }: DifficultyItemProps) {
 				bgColor="blue.700"
 				color="gray.100"
 				onClick={next}
-				_focus={{}}
 			/>
 		</Button>
 	);
