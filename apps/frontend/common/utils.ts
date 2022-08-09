a2dbe2aed04229eb0ce9c3ac44871d2a17be4056
Zeilar <philip_angelin@hotmail.com>
2022-08-09 23:52:36 +0200

    fix: actual new letter every time

--- apps/frontend/common/utils.ts
+++ apps/frontend/common/utils.ts
@@ -1,9 +1,9 @@
 import { alphabet } from "./constants";
 
-export function randomUniqueLetter(currentLetter: string | null) {
+export function randomUniqueLetter(currentLetter: string | null): string {
 	const newLetter = alphabet[Math.floor(alphabet.length * Math.random())];
 	if (newLetter === currentLetter) {
-		randomUniqueLetter(currentLetter);
+		return randomUniqueLetter(currentLetter);
 	}
 	return newLetter;
 }
