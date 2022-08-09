e2596245e293020d07c3ad0dee9d275e53e8878f
Zeilar <philip_angelin@hotmail.com>
2022-08-09 23:53:54 +0200

    refactor: ternary

--- apps/frontend/common/utils.ts
+++ apps/frontend/common/utils.ts
@@ -2,8 +2,5 @@ import { alphabet } from "./constants";
 
 export function randomUniqueLetter(currentLetter: string | null): string {
 	const newLetter = alphabet[Math.floor(alphabet.length * Math.random())];
-	if (newLetter === currentLetter) {
-		return randomUniqueLetter(currentLetter);
-	}
-	return newLetter;
+	return newLetter !== currentLetter ? newLetter : randomUniqueLetter(currentLetter);
 }
