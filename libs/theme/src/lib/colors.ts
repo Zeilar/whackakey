97b93e7f73f347ea0b9b749285d3342cdd3203d0
Zeilar <philip_angelin@hotmail.com>
2022-08-18 13:57:46 +0200

    feat: remove colors

--- libs/theme/src/lib/colors.ts
+++
@@ -1,5 +0,0 @@
-import { theme } from "@chakra-ui/react";
-
-export const colors = {
-	player: theme.colors.blue,
-};
--- libs/theme/src/lib/index.ts
+++ libs/theme/src/lib/index.ts
@@ -1,5 +1,4 @@
 import { extendTheme } from "@chakra-ui/react";
-import { colors } from "./colors";
 import * as components from "./components";
 import { config } from "./config";
 import { fonts } from "./fonts";
@@ -9,7 +8,6 @@ import { textStyles } from "./textStyles";
 const theme = extendTheme({
 	config,
 	fonts,
-	colors,
 	components: {
 		...components, // This needs to be spread into a new object, no idea why
 	},
